2ee6ac5861c6227c33cbd028caa4ce33e1426121
sergeythrees <sergeythrees@gmail.com>
2016-10-19 10:58:35 +0300

    исправил определение SearchFunc

--- 2/vector/VectorProcessor.cpp
+++ 2/vector/VectorProcessor.cpp
@@ -1,11 +1,9 @@
 #include "stdafx.h"
 #include <vector>
 #include <functional>
-#include <algorithm>
 #include <boost/range/algorithm/transform.hpp>
 #include <boost/phoenix.hpp>
 #include <boost/range/algorithm/sort.hpp>
-#include <boost/range/algorithm/min_element.hpp>
 #include "VectorProcessor.h"
 
 using namespace std;
@@ -15,19 +13,13 @@ using boost::transform;
 using boost::sort;
 using boost::min_element;
 
-//bool IsLess(double a, double b)
-//{
-//	return a < b;
-//}
+
 void ProcessVector(std::vector<double> & numbers, 
 	std::function<bool(double a, double b)> PredicateFunc, 
-	std::function<auto(vector<double>)> SearchFunc,
+	std::function<vector<double>::iterator(vector<double>::iterator, vector<double>::iterator)> SearchFunc,
 	std::function<double(double a, double b)> TransformFunc)
 {
 	sort(numbers, PredicateFunc);
-	double minNumber = *SearchFunc(numbers);
-	//double minNumber = 
-		//*SearchElementFunc(numbers.begin(),numbers.end());
-	//double minNumber =  *(numbers.begin());
-	transform(numbers, numbers.begin(), bind(TransformFunc, _1, minNumber));
+	double foundNumber = *SearchFunc(numbers);
+	transform(numbers, numbers.begin(), bind(TransformFunc, _1, foundNumber));
 }
