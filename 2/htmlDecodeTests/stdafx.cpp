65e37f1ef66965200ab482958648dbe938deeb24
sergeythrees <sergeythrees@gmail.com>
2016-11-03 10:42:12 +0300

    добавил класс форматирования тестов

--- 2/htmlDecodeTests/stdafx.cpp
+++ 2/htmlDecodeTests/stdafx.cpp
@@ -1,8 +1,10 @@
-// stdafx.cpp: исходный файл, содержащий только стандартные включаемые модули
-// htmlDecodeTests.pch будет предкомпилированным заголовком
-// stdafx.obj будет содержать предварительно откомпилированные сведения о типе
+// stdafx.cpp : source file that includes just the standard includes
+// vector_tests.pch will be the pre-compiled header
+// stdafx.obj will contain the pre-compiled type information
 
 #include "stdafx.h"
+#define BOOST_TEST_NO_MAIN 
+#include <boost/test/included/unit_test.hpp>
 
-// TODO: Установите ссылки на любые требующиеся дополнительные заголовки в файле STDAFX.H
-// , а не в данном файле
+// TODO: reference any additional headers you need in STDAFX.H
+// and not in this file
--- 2/htmlDecodeTests/stdafx.h
+++ 2/htmlDecodeTests/stdafx.h
@@ -1,6 +1,6 @@
-// stdafx.h: включаемый файл дл€ стандартных системных включаемых файлов
-// или включаемых файлов дл€ конкретного проекта, которые часто используютс€, но
-// не часто измен€ютс€
+// stdafx.h : include file for standard system include files,
+// or project specific include files that are used frequently, but
+// are changed infrequently
 //
 
 #pragma once
@@ -10,11 +10,9 @@
 #include <stdio.h>
 #include <tchar.h>
 
+#define BOOST_TEST_INCLUDED
+#pragma warning (disable:4702) // To disable 4702 warning in boost
+#include <boost/test/unit_test.hpp>
 #include <boost/algorithm/string/replace.hpp>
 
-#define BOOST_TEST_INCLUDED
-#pragma warning (disable: 4702)
-#include <boost/test/unit_test.hpp>
-
-
-// TODO: ”становите здесь ссылки на дополнительные заголовки, требующиес€ дл€ программы
+// TODO: reference additional headers your program requires here
