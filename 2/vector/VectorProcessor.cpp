66fbb7f1e25272d45f6cb033f98e365520951776
sergeythrees <sergeythrees@gmail.com>
2016-11-03 11:06:12 +0300

    заменил boost::phoenix на std::placeholders

--- 2/vector/VectorProcessor.cpp
+++ 2/vector/VectorProcessor.cpp
@@ -1,18 +1,19 @@
 #include "stdafx.h"
 #include <vector>
+#include <functional>
 #include <boost/range/algorithm/min_element.hpp>
 #include <boost/range/algorithm/transform.hpp>
-#include <boost/phoenix.hpp>
 #include "VectorProcessor.h"
 
+using namespace std;
+using namespace std::placeholders;
 using namespace boost;
-using namespace boost::phoenix::placeholders;
 
-void ProcessVector(std::vector<double> & numbers)
+void ProcessVector(vector<double> & numbers)
 {
 	if (numbers.size() != 0)
 	{
 		double minElement = *(min_element(numbers));
-		boost::transform(numbers, numbers.begin(), arg1 * minElement);
+		boost::transform(numbers, numbers.begin(), bind(multiplies<double>(), _1, minElement));
 	}
 }
