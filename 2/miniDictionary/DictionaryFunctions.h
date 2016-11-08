f912990a5a8d240daaa4f13707b520b80e70788b
sergeythrees <sergeythrees@gmail.com>
2016-11-08 03:18:43 +0300

    сменил локализацию, заменил string на wstring для корректной работы с кириллицей

--- 2/miniDictionary/DictionaryFunctions.h
+++ 2/miniDictionary/DictionaryFunctions.h
@@ -2,6 +2,6 @@
 #include "stdafx.h"
 #include <map>
 
-typedef std::multimap <std::string, std::string> Dictionary;
+typedef std::multimap <std::wstring, std::wstring> Dictionary;
 void GetDictionaryFromFile(std::fstream &fs, Dictionary &sset2);
 void SaveDictionaryToFile(std::fstream &fs, Dictionary &temp);
--- 2/miniDictionary/main.cpp
+++ 2/miniDictionary/main.cpp
@@ -1,21 +1,22 @@
 #include "stdafx.h"
 #include "DictionaryFunctions.h"
+#include <locale>
 #include <iostream>
+#include <fstream>
 #include <string>
 #include <map>
-#include <fstream>
-
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	std::fstream fs;
 	Dictionary dictionary;
+	Dictionary dict{ { L"", L"" } ,{ L"fgdgdfg", L"ûגûגאûא" }, { L"ûגאגא", L"גאûגא" } , { L"sdsd", L"sds" } };
 	fs.open("סכמגאנü", std::ios::out | std::ios::binary);
-	SaveDictionaryToFile(fs, dictionary);
+	SaveDictionaryToFile(fs, dict);
 	fs.open("סכמגאנü", std::ios::in | std::ios::binary);
 	GetDictionaryFromFile(fs, dictionary);
 	for (auto const& current : dictionary)
-		std::cout << current.first << " : " << current.second << '\n';
+		std::wcout << current.first << " : " << current.second << '\n';
     return 0;
 }
 
