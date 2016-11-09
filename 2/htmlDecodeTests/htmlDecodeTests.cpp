1cb24128027e84276b0222d337c8afdf7c1680f3
sergeythrees <sergeythrees@gmail.com>
2016-11-09 11:37:44 +0300

    вынес повторяющийся код в функцию VerifyHtmlDecode

--- 2/htmlDecodeTests/htmlDecodeTests.cpp
+++ 2/htmlDecodeTests/htmlDecodeTests.cpp
@@ -3,42 +3,36 @@
 
 using namespace std;
 
+bool VerifyHtmlDecode(string input, string expectedresult)
+{
+	return expectedresult == DecodeHtmlText(input);
+}
+
 BOOST_AUTO_TEST_SUITE(HtmlDecode_function)
 	
 BOOST_AUTO_TEST_CASE(return_empty_string_from_empty_string)
 {
-	const string expectedResult = "";
-	string input = "";
-	auto result = DecodeHtmlText(input);
-
-	BOOST_CHECK(result == expectedResult);
+	BOOST_CHECK(VerifyHtmlDecode("", ""));
 }
 
 BOOST_AUTO_TEST_CASE(doesnt_change_string_without_html_entities)
 {
-	const string expectedResult = "The quick brown fox jumps over the lazy dog";
-	string input = "The quick brown fox jumps over the lazy dog";
-	auto result = DecodeHtmlText(input);
-
-	BOOST_CHECK_EQUAL(result, expectedResult);
+	BOOST_CHECK(VerifyHtmlDecode("The quick brown fox jumps over the lazy dog", 
+				     "The quick brown fox jumps over the lazy dog"));
 }
 
 BOOST_AUTO_TEST_CASE(replaces_each_html_entity_with_corresponding_character)
 {
-	const string expectedResult = "Cat <says> \"Meow. M&M's";
-	string input = "Cat &lt;says&gt; &quot;Meow. M&amp;M&apos;s";
-	auto result = DecodeHtmlText(input);
-
-	BOOST_CHECK_EQUAL(result, expectedResult);
+	BOOST_CHECK(VerifyHtmlDecode("Cat &lt;says&gt; &quot;Meow. M&amp;M&apos;s", 
+				     "Cat <says> \"Meow. M&M's"));
 }
 
 BOOST_AUTO_TEST_CASE(correct_replaces_each_html_entity_recursive_character)
 {
-	const string expectedResult = "Cat &lt<says> \"Meow&qu\"ot;. M&M's\\\ Cat <says> \"Meow\".";
-	string input = "Cat &lt&lt;says&gt; &quot;Meow&qu&quot;ot;. M&amp;M&apos;s\\\ Cat &lt;says&gt; &quot;Meow&quot;.";
-	auto result = DecodeHtmlText(input);
-
-	BOOST_CHECK_EQUAL(result, expectedResult);
+	BOOST_CHECK(VerifyHtmlDecode(
+		"Cat &lt&lt;says&gt; &quot;Meow&qu&quot;ot;. "
+		+"M&amp;M&apos;s\\\ Cat &lt;says&gt; &quot;Meow&quot;.", 
+		"Cat &lt<says> \"Meow&qu\"ot;. M&M's\\\ Cat <says> \"Meow\"."));
 }
 
 BOOST_AUTO_TEST_SUITE_END()
