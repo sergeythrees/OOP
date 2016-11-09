46b17057b0d4f6b2c0dacf7da8e5892051e89373
sergeythrees <sergeythrees@gmail.com>
2016-11-09 11:12:21 +0300

    заменил обычную проверку на проверку с заданной точностью

--- 2/vectorTests/vectorProcessorTests.cpp
+++ 2/vectorTests/vectorProcessorTests.cpp
@@ -2,11 +2,13 @@
 #include "..\vector\VectorProcessor.h"
 #include <boost/test/output/compiler_log_formatter.hpp>
 #include <boost/algorithm/string/replace.hpp>
+
 using namespace std;
 
-bool VectorsAreEqual(vector<double> const& x, vector<double> const& y)
+bool VerifyProcessVector(vector<double> input, vector<double> expectedresult)
 {
-	return x == y;
+	ProcessVector(input);
+	return expectedresult == input;
 }
 
 BOOST_AUTO_TEST_SUITE(ProcessVector_check)
@@ -15,32 +17,15 @@ BOOST_AUTO_TEST_SUITE(ProcessVector_check)
 BOOST_AUTO_TEST_CASE(makes_empty_vector_from_empty_vector)
 {
 	vector<double> emptyVector;
-	ProcessVector(emptyVector);
-	BOOST_CHECK(emptyVector.empty());
+	BOOST_CHECK(VerifyProcessVector(emptyVector, emptyVector));
 }
 
 
-BOOST_AUTO_TEST_CASE(mult_all_elements_at_zero)
+BOOST_AUTO_TEST_CASE(multiple_all_elements_at_min_element)
 {
-	vector<double> numbers = { 0, 1, 2, 3 };
-	ProcessVector(numbers);
-	BOOST_CHECK(VectorsAreEqual(numbers, { 0, 0, 0, 0 }));
+	BOOST_CHECK_CLOSE(VerifyProcessVector(
+		{ -1.5689, 0, -25.5689, 33.435, 2, 1 }, 
+		{ 40.12, 0, 653.8, -854.9, -51.14, -25.57 }), 4);
 }
-
-
-BOOST_AUTO_TEST_CASE(mult_all_elements_at_negative_value)
-{
-	vector<double> numbers = { -1, 0, 1, 2, 3 };
-	ProcessVector(numbers);
-	BOOST_CHECK(VectorsAreEqual(numbers, { 1, 0, -1, -2, -3 }));
-}
-
-BOOST_AUTO_TEST_CASE(process_double_numbers)
-{
-	vector<double> numbers = { -2.00, 5.3435, -25.5689, -3 };
-	ProcessVector(numbers);
-	BOOST_CHECK(VectorsAreEqual(numbers, { 51.137, -136.627, 653.768, 76.706 }));
-}
-
 
 BOOST_AUTO_TEST_SUITE_END()
