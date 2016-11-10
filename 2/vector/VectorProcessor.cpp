ee2a636d163469f5d2173da3344697a267975dca
sergeythrees <sergeythrees@gmail.com>
2016-11-10 04:32:38 +0300

    реализовал тесты с заданной точностью

--- 2/vector/VectorProcessor.cpp
+++ 2/vector/VectorProcessor.cpp
@@ -1,10 +1,12 @@
 #include "stdafx.h"
 #include <vector>
+#include <functional>
 #include <boost/range/algorithm/min_element.hpp>
 #include <boost/range/algorithm/transform.hpp>
 #include "VectorProcessor.h"
 
 using namespace std;
+using namespace std::placeholders;
 using namespace boost;
 
 void ProcessVector(vector<double> & numbers)
--- 2/vectorTests/vectorProcessorTests.cpp
+++ 2/vectorTests/vectorProcessorTests.cpp
@@ -3,29 +3,40 @@
 #include <boost/test/output/compiler_log_formatter.hpp>
 #include <boost/algorithm/string/replace.hpp>
 
+#define CHECK_CLOSE_COLLECTION(aa, bb, tolerance) { \
+    using std::distance; \
+    using std::begin; \
+    using std::end; \
+    auto a = begin(aa), ae = end(aa); \
+    auto b = begin(bb); \
+    BOOST_REQUIRE_EQUAL(distance(a, ae), distance(b, end(bb))); \
+    for(; a != ae; ++a, ++b) { \
+        BOOST_CHECK_CLOSE(*a, *b, tolerance); \
+    } \
+}
+
 using namespace std;
 
-bool VerifyProcessVector(vector<double> input, vector<double> expectedresult)
+void VerifyProcessVector(vector<double> resultVector, vector<double> expectedResult)
 {
-	ProcessVector(input);
-	return expectedresult == input;
+	ProcessVector(resultVector);
+	CHECK_CLOSE_COLLECTION(resultVector, expectedResult, 0.1);
 }
 
 BOOST_AUTO_TEST_SUITE(ProcessVector_check)
 
 
-BOOST_AUTO_TEST_CASE(makes_empty_vector_from_empty_vector)
+BOOST_AUTO_TEST_CASE(should_makes_empty_vector_from_empty_vector)
 {
-	vector<double> emptyVector;
-	BOOST_CHECK(VerifyProcessVector(emptyVector, emptyVector));
+	VerifyProcessVector({}, {});
 }
 
 
-BOOST_AUTO_TEST_CASE(multiple_all_elements_at_min_element)
+BOOST_AUTO_TEST_CASE(should_multiple_all_elements_at_min_element)
 {
-	BOOST_CHECK_CLOSE(VerifyProcessVector(
+	VerifyProcessVector(	
 		{ -1.5689, 0, -25.5689, 33.435, 2, 1 }, 
-		{ 40.12, 0, 653.8, -854.9, -51.14, -25.57 }), 4);
+		{ 40.12, 0, 653.8, -854.9, -51.14, -25.57 }	);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
