ae6ace3074c6a759590e8455af83170c0bc97123
sergeythrees <sergeythrees@gmail.com>
2016-11-10 03:32:36 +0300

    подправил названия тестов

--- 2/htmlDecodeTests/htmlDecodeTests.cpp
+++ 2/htmlDecodeTests/htmlDecodeTests.cpp
@@ -10,28 +10,27 @@ bool VerifyHtmlDecode(string input, string expectedresult)
 
 BOOST_AUTO_TEST_SUITE(HtmlDecode_function)
 	
-BOOST_AUTO_TEST_CASE(return_empty_string_from_empty_string)
+BOOST_AUTO_TEST_CASE(should_return_empty_string_from_empty_string)
 {
 	BOOST_CHECK(VerifyHtmlDecode("", ""));
 }
 
-BOOST_AUTO_TEST_CASE(doesnt_change_string_without_html_entities)
+BOOST_AUTO_TEST_CASE(should_not_change_string_without_html_entities)
 {
 	BOOST_CHECK(VerifyHtmlDecode("The quick brown fox jumps over the lazy dog", 
 				     "The quick brown fox jumps over the lazy dog"));
 }
 
-BOOST_AUTO_TEST_CASE(replaces_each_html_entity_with_corresponding_character)
+BOOST_AUTO_TEST_CASE(should_replaces_each_html_entity_with_corresponding_character)
 {
 	BOOST_CHECK(VerifyHtmlDecode("Cat &lt;says&gt; &quot;Meow. M&amp;M&apos;s", 
 				     "Cat <says> \"Meow. M&M's"));
 }
 
-BOOST_AUTO_TEST_CASE(correct_replaces_each_html_entity_recursive_character)
+BOOST_AUTO_TEST_CASE(should_correctly_replaces_recursived_html_entities)
 {
 	BOOST_CHECK(VerifyHtmlDecode(
-		"Cat &lt&lt;says&gt; &quot;Meow&qu&quot;ot;. "
-		+"M&amp;M&apos;s\\\ Cat &lt;says&gt; &quot;Meow&quot;.", 
+		"Cat &lt&lt;says&gt; &quot;Meow&qu&quot;ot;. M&amp;M&apos;s\\\ Cat &lt;says&gt; &quot;Meow&quot;.", 
 		"Cat &lt<says> \"Meow&qu\"ot;. M&M's\\\ Cat <says> \"Meow\"."));
 }
 
