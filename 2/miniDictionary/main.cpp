d97e37655c040e18ec29567053df16f2ce7e0cb4
sergeythrees <sergeythrees@gmail.com>
2016-11-11 10:53:16 +0300

    настроил кодировку ввода и вывода консоли для корректной работы с русскими символами

--- 2/miniDictionary/main.cpp
+++ 2/miniDictionary/main.cpp
@@ -19,8 +19,9 @@ void MainLoop(Dictionary dictionary);
 
 int main()
 {
-	setlocale(LC_ALL, "Russian");
-
+	setlocale(LC_ALL, "rus");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	//std::fstream file;
 	//Dictionary dictionary;
 	//Dictionary dict{ { "", "" } ,{ "fgdgdfg", "ûגûגאûא" },{ "ûגאגא", "גאûגא" } ,{ "sdsd", "sds" } };
