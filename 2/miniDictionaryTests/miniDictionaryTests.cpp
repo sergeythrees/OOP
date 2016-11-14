a5b610f00be7ddf0e2805eccb2cd3038d64fcf11
sergeythrees <sergeythrees@gmail.com>
2016-11-14 16:38:50 +0300

    поправил названия тестов

--- 2/miniDictionaryTests/miniDictionaryTests.cpp
+++ 2/miniDictionaryTests/miniDictionaryTests.cpp
@@ -1,17 +1,20 @@
 #include "stdafx.h"
+#include <sstream>
 #include "../miniDictionary/DictionaryFunctions.h"
 
 using namespace std;
-
 
 /////////////////////////////////////////////////////////////////////////////////////////
 
 
 BOOST_AUTO_TEST_SUITE(ToLower_function)
 
-BOOST_AUTO_TEST_CASE(should_replace_to_lower_russian_letters)
+BOOST_AUTO_TEST_CASE(should_replace_russian_and_english_letters_in_word_to_lower)
 {
+	BOOST_CHECK(ToLower("APPLE") == "apple"); 
+	BOOST_CHECK(ToLower("Apple") == "apple");
 	BOOST_CHECK(ToLower("ЯБЛОКО") == "яблоко");
+	BOOST_CHECK(ToLower("Яблоко") == "яблоко");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -25,7 +28,6 @@ BOOST_AUTO_TEST_SUITE(IsRussianWord_function)
 BOOST_AUTO_TEST_CASE(should_return_false_if_word_contain_not_russian_charaster)
 {
 	BOOST_CHECK(IsWordRussian("apple") == false);
-	BOOST_CHECK(IsWordRussian("APPLE") == false);
 	BOOST_CHECK(IsWordRussian("ЯблокоApple") == false);
 	BOOST_CHECK(IsWordRussian("Яблоко") == true);
 }
@@ -72,10 +74,9 @@ BOOST_AUTO_TEST_SUITE_END()
 /////////////////////////////////////////////////////////////////////////////////////////
 
 
-
 BOOST_AUTO_TEST_SUITE(InsertNewPair_function)
 
-BOOST_AUTO_TEST_CASE(should_insert_new_pair_from_english_and_russian_words_into_dictionary)
+BOOST_AUTO_TEST_CASE(should_insert_new_translation_from_english_to_russian_into_dictionary)
 {
 	multimap<string, string> EmptyDictionary;
 	multimap<string, string> Dictionary =
@@ -90,27 +91,50 @@ BOOST_AUTO_TEST_CASE(should_insert_new_pair_from_english_and_russian_words_into_
 	BOOST_CHECK(Dictionary == UpdatedDictionary);
 }
 
+BOOST_AUTO_TEST_CASE(should_insert_new_translation_from_russian_to_english_into_dictionary)
+{
+	multimap<string, string> EmptyDictionary;
+	multimap<string, string> Dictionary =
+	{ { "key" , "клавиша" } };
+	multimap<string, string> UpdatedDictionary =
+	{ { "key" , "клавиша" },{ "key" , "ключ" }, };
+
+	InsertNewPair("клавиша", "key", EmptyDictionary);
+	BOOST_CHECK(EmptyDictionary == Dictionary);
+
+	InsertNewPair("ключ", "key", Dictionary);
+	BOOST_CHECK(Dictionary == UpdatedDictionary);
+}
+
 BOOST_AUTO_TEST_SUITE_END()
 
-//BOOST_AUTO_TEST_CASE(replace_required_substrings_once)
-//{
-//	vector<pair<string, string>> htmlMap = { { "&quot;", "\"" },{ "&apos;", "'" },{ "&lt;", "<" },{ "&gt;", ">" },{ "&amp;", "&" } };
-//	const string expectedResult = "Cat <says> \"Meow. M&M's";
-//	string input = "Cat &lt;says&gt; &quot;Meow. M&amp;M&apos;s";
-//	auto result = DecodeHtmlText(input);
-//
-//	BOOST_CHECK(result == expectedResult);
-//}
-//
-//BOOST_AUTO_TEST_CASE(replace_required_repeated_substrings)
-//{
-//	vector<pair<string, string>> htmlMap = { { "&quot;", "\"" },{ "&apos;", "'" },{ "&lt;", "<" },{ "&gt;", ">" },{ "&amp;", "&" } };
-//
-//	const string expectedResult = "Cat <says> \"Meow\". M&M's\\\ Cat <says> \"Meow\".";
-//	string input = "Cat &lt;says&gt; &quot;Meow&quot;. M&amp;M&apos;s\\\ Cat &lt;says&gt; &quot;Meow&quot;.";
-//	auto result = DecodeHtmlText(input);
-//
-//	BOOST_CHECK(result == expectedResult);
-//}
+
+/////////////////////////////////////////////////////////////////////////////////////////
 
 
+BOOST_AUTO_TEST_SUITE(FillDictionaryFromInputStream_function)
+
+BOOST_AUTO_TEST_CASE(should_correctly_fill_dictionary_from_stringstream)
+{
+	multimap<string, string> EmptyDictionary;
+	multimap<string, string> Dictionary =
+	{ { "key" , "клавиша" } };
+	multimap<string, string> UpdatedDictionary =
+	{ { "key" , "клавиша" },{ "key" , "ключ" }, };
+
+	stringstream input;
+	FillDictionaryFromInputStream(input, EmptyDictionary);
+	BOOST_CHECK(EmptyDictionary.empty());
+
+	input << "key" << endl << "клавиша" << endl;
+	FillDictionaryFromInputStream(input, EmptyDictionary);
+	BOOST_CHECK(EmptyDictionary == Dictionary);
+
+	input << "key" << endl << "ключ" << endl;
+	FillDictionaryFromInputStream(input, EmptyDictionary);
+	BOOST_CHECK(EmptyDictionary == UpdatedDictionary);
+}
+
+
+
+BOOST_AUTO_TEST_SUITE_END()
