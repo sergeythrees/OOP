f441ef7a233ecf5c784c6ebad01f87410e4c7676
sergeythrees <sergeythrees@gmail.com>
2016-11-16 16:32:27 +0300

    поправил структуру файлов

--- 3/Car/Car.cpp
+++ 3/Car/Car.cpp
@@ -1,12 +1,47 @@
 #include "stdafx.h"
 #include "Car.h"
 
-
-CCar::CCar()
+bool CCar::isTurnedOn() const
 {
+	return false;
 }
 
-
-CCar::~CCar()
+Direction CCar::GetDirection() const
 {
+	return Direction();
+}
+
+int CCar::GetSpeed() const
+{
+	return 0;
+}
+
+int CCar::GetGear() const
+{
+	return 0;
+}
+
+bool CCar::TurnOnEngine()
+{
+	return false;
+}
+
+bool CCar::TurnOffEngine()
+{
+	return false;
+}
+
+bool CCar::SetGear(int gear)
+{
+	return false;
+}
+
+bool CCar::SetSpeed(int speed)
+{
+	return false;
+}
+
+bool CCar::IsSpeedInRange(int speed, Gear currentGear) const
+{
+	return false;
 }
