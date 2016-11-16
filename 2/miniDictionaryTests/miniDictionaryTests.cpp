84be85b05454b96273730071bf6a8122a4570379
sergeythrees <sergeythrees@gmail.com>
2016-11-16 11:35:21 +0300

    исправил синтаксические ошибки

--- 2/miniDictionaryTests/miniDictionaryTests.cpp
+++ 2/miniDictionaryTests/miniDictionaryTests.cpp
@@ -1,5 +1,4 @@
 #include "stdafx.h"
-#include <sstream>
 #include "../miniDictionary/DictionaryFunctions.h"
 
 using namespace std;
@@ -42,7 +41,7 @@ BOOST_AUTO_TEST_SUITE(GetAllTranslation_function)
 
 BOOST_AUTO_TEST_CASE(should_return_all_translations_of_russian_word_to_english)
 {
-	multimap<string, string> Dictionary = 
+	Dictionary dictionary =
 	{ 
 		{ "apple" , "ÿáëîêî" }, { "black" , "òåìíûé" }, { "dark" , "òåìíûé" } ,	
 		{ "key" , "êëþ÷" }, { "key" , "êëàâèøà" }, {"orange" , "àïåëüñèí" }, 
@@ -50,13 +49,13 @@ BOOST_AUTO_TEST_CASE(should_return_all_translations_of_russian_word_to_english)
 	};
 
 	vector<string> translations = { "black" , "dark" , "shadow"};
-	BOOST_CHECK(GetAllTranslations("òåìíûé", Dictionary) == translations);
-	BOOST_CHECK(GetAllTranslations("Òåìíûé", Dictionary) == translations);
+	BOOST_CHECK(GetAllTranslations("òåìíûé", dictionary) == translations);
+	BOOST_CHECK(GetAllTranslations("Òåìíûé", dictionary) == translations);
 }
 
 BOOST_AUTO_TEST_CASE(should_return_all_translations_of_english_word_to_russian)
 {
-	multimap<string, string> Dictionary =
+	Dictionary dictionary =
 	{
 		{ "apple" , "ÿáëîêî" },{ "black" , "òåìíûé" },{ "dark" , "òåìíûé" } ,
 		{ "key" , "êëþ÷" },{ "key" , "êëàâèøà" },{ "orange" , "àïåëüñèí" },
@@ -64,8 +63,8 @@ BOOST_AUTO_TEST_CASE(should_return_all_translations_of_english_word_to_russian)
 	};
 
 	vector<string> translations = { "êëþ÷" , "êëàâèøà"};
-	BOOST_CHECK(GetAllTranslations("key", Dictionary) == translations);
-	BOOST_CHECK(GetAllTranslations("Key", Dictionary) == translations);
+	BOOST_CHECK(GetAllTranslations("key", dictionary) == translations);
+	BOOST_CHECK(GetAllTranslations("Key", dictionary) == translations);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -78,32 +77,32 @@ BOOST_AUTO_TEST_SUITE(InsertNewPair_function)
 
 BOOST_AUTO_TEST_CASE(should_insert_new_translation_from_english_to_russian_into_dictionary)
 {
-	multimap<string, string> EmptyDictionary;
-	multimap<string, string> Dictionary =
+	Dictionary EmptyDictionary;
+	Dictionary dictionary =
 	{ { "key" , "êëàâèøà" } };
-	multimap<string, string> UpdatedDictionary =
-	{ { "key" , "êëàâèøà" }, { "key" , "êëþ÷" },};
+	Dictionary UpdatedDictionary =
+	{ { "key" , "êëàâèøà" }, { "key" , "êëþ÷" }};
 
 	InsertNewPair("key", "êëàâèøà", EmptyDictionary);
-	BOOST_CHECK(EmptyDictionary == Dictionary);
+	BOOST_CHECK(EmptyDictionary == dictionary);
 
-	InsertNewPair("key", "êëþ÷", Dictionary);
-	BOOST_CHECK(Dictionary == UpdatedDictionary);
+	InsertNewPair("key", "êëþ÷", dictionary);
+	BOOST_CHECK(dictionary == UpdatedDictionary);
 }
 
 BOOST_AUTO_TEST_CASE(should_insert_new_translation_from_russian_to_english_into_dictionary)
 {
-	multimap<string, string> EmptyDictionary;
-	multimap<string, string> Dictionary =
+	Dictionary EmptyDictionary;
+	Dictionary dictionary =
 	{ { "key" , "êëàâèøà" } };
-	multimap<string, string> UpdatedDictionary =
-	{ { "key" , "êëàâèøà" },{ "key" , "êëþ÷" }, };
+	Dictionary UpdatedDictionary =
+	{ { "key" , "êëàâèøà" },{ "key" , "êëþ÷" }};
 
 	InsertNewPair("êëàâèøà", "key", EmptyDictionary);
-	BOOST_CHECK(EmptyDictionary == Dictionary);
+	BOOST_CHECK(EmptyDictionary == dictionary);
 
-	InsertNewPair("êëþ÷", "key", Dictionary);
-	BOOST_CHECK(Dictionary == UpdatedDictionary);
+	InsertNewPair("êëþ÷", "key", dictionary);
+	BOOST_CHECK(dictionary == UpdatedDictionary);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -116,8 +115,8 @@ BOOST_AUTO_TEST_SUITE(FillDictionaryFromInputStream_function)
 
 BOOST_AUTO_TEST_CASE(should_makes_empty_dictionary_from_empty_file)
 {
-	Dictionary EmptyDictionary;
 	stringstream input;
+	Dictionary EmptyDictionary;
 
 	FillDictionaryFromInputStream(input, EmptyDictionary);
 	BOOST_CHECK(EmptyDictionary.empty());
@@ -125,14 +124,15 @@ BOOST_AUTO_TEST_CASE(should_makes_empty_dictionary_from_empty_file)
 
 BOOST_AUTO_TEST_CASE(should_add_new_pair_to_non_empty_dictionary)
 {
-	Dictionary Dictionary =
+	stringstream input;
+	Dictionary dictionary =
 	{ { "key", "клавиша" } };
 	Dictionary UpdatedDictionary =
-	{ { "key", "клавиша" }, { "key", "ключ" }, };
+	{ { "key", "клавиша" }, { "key", "ключ" } };
 
 	input << "key" << endl << "ключ" << endl;
-	FillDictionaryFromInputStream(input, EmptyDictionary);
-	BOOST_CHECK(Dictionary == UpdatedDictionary);
+	FillDictionaryFromInputStream(input, dictionary);
+	BOOST_CHECK(dictionary == UpdatedDictionary);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
