04861b699e836a805872d9e084184de28faa9e13
sergeythrees <sergeythrees@gmail.com>
2016-11-16 16:07:24 +0300

    подправил структуру тестов

--- 2/miniDictionaryTests/miniDictionaryTests.cpp
+++ 2/miniDictionaryTests/miniDictionaryTests.cpp
@@ -12,8 +12,8 @@ BOOST_AUTO_TEST_CASE(should_replace_russian_and_english_letters_in_word_to_lower
 {
 	BOOST_CHECK(ToLower("APPLE") == "apple"); 
 	BOOST_CHECK(ToLower("Apple") == "apple");
-	BOOST_CHECK(ToLower("ßÁËÎÊÎ") == "ÿáëîêî");
-	BOOST_CHECK(ToLower("ßáëîêî") == "ÿáëîêî");
+	BOOST_CHECK(ToLower("ЯБЛОКО") == "яблоко");
+	BOOST_CHECK(ToLower("Яблоко") == "яблоко");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -27,8 +27,8 @@ BOOST_AUTO_TEST_SUITE(IsRussianWord_function)
 BOOST_AUTO_TEST_CASE(should_return_false_if_word_contain_not_russian_charaster)
 {
 	BOOST_CHECK(IsWordRussian("apple") == false);
-	BOOST_CHECK(IsWordRussian("ßáëîêîApple") == false);
-	BOOST_CHECK(IsWordRussian("ßáëîêî") == true);
+	BOOST_CHECK(IsWordRussian("ЯблокоApple") == false);
+	BOOST_CHECK(IsWordRussian("Яблоко") == true);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
@@ -43,26 +43,26 @@ BOOST_AUTO_TEST_CASE(should_return_all_translations_of_russian_word_to_english)
 {
 	Dictionary dictionary =
 	{ 
-		{ "apple" , "ÿáëîêî" }, { "black" , "òåìíûé" }, { "dark" , "òåìíûé" } ,	
-		{ "key" , "êëþ÷" }, { "key" , "êëàâèøà" }, {"orange" , "àïåëüñèí" }, 
-		{ "shadow" , "òåìíûé" } 
+		{ "apple" , "яблоко" }, { "black" , "темный" }, { "dark" , "темный" } ,	
+		{ "key" , "ключ" }, { "key" , "клавиша" }, {"orange" , "апельсин" }, 
+		{ "shadow" , "темный" } 
 	};
 
 	vector<string> translations = { "black" , "dark" , "shadow"};
-	BOOST_CHECK(GetAllTranslations("òåìíûé", dictionary) == translations);
-	BOOST_CHECK(GetAllTranslations("Òåìíûé", dictionary) == translations);
+	BOOST_CHECK(GetAllTranslations("темный", dictionary) == translations);
+	BOOST_CHECK(GetAllTranslations("Темный", dictionary) == translations);
 }
 
 BOOST_AUTO_TEST_CASE(should_return_all_translations_of_english_word_to_russian)
 {
 	Dictionary dictionary =
 	{
-		{ "apple" , "ÿáëîêî" },{ "black" , "òåìíûé" },{ "dark" , "òåìíûé" } ,
-		{ "key" , "êëþ÷" },{ "key" , "êëàâèøà" },{ "orange" , "àïåëüñèí" },
-		{ "shadow" , "òåìíûé" }
+		{ "apple" , "яблоко" },{ "black" , "темный" },{ "dark" , "темный" } ,
+		{ "key" , "ключ" },{ "key" , "клавиша" },{ "orange" , "апельсин" },
+		{ "shadow" , "темный" }
 	};
 
-	vector<string> translations = { "êëþ÷" , "êëàâèøà"};
+	vector<string> translations = { "ключ" , "клавиша"};
 	BOOST_CHECK(GetAllTranslations("key", dictionary) == translations);
 	BOOST_CHECK(GetAllTranslations("Key", dictionary) == translations);
 }
@@ -79,14 +79,14 @@ BOOST_AUTO_TEST_CASE(should_insert_new_translation_from_english_to_russian_into_
 {
 	Dictionary EmptyDictionary;
 	Dictionary dictionary =
-	{ { "key" , "êëàâèøà" } };
+	{ { "key" , "клавиша" } };
 	Dictionary UpdatedDictionary =
-	{ { "key" , "êëàâèøà" }, { "key" , "êëþ÷" }};
+	{ { "key" , "клавиша" },{ "key" , "ключ" } };
 
-	InsertNewPair("key", "êëàâèøà", EmptyDictionary);
+	InsertNewPair("key", "клавиша", EmptyDictionary);
 	BOOST_CHECK(EmptyDictionary == dictionary);
 
-	InsertNewPair("key", "êëþ÷", dictionary);
+	InsertNewPair("key", "ключ", dictionary);
 	BOOST_CHECK(dictionary == UpdatedDictionary);
 }
 
@@ -94,14 +94,14 @@ BOOST_AUTO_TEST_CASE(should_insert_new_translation_from_russian_to_english_into_
 {
 	Dictionary EmptyDictionary;
 	Dictionary dictionary =
-	{ { "key" , "êëàâèøà" } };
+	{ { "key" , "клавиша" } };
 	Dictionary UpdatedDictionary =
-	{ { "key" , "êëàâèøà" },{ "key" , "êëþ÷" }};
+	{ { "key" , "клавиша" },{ "key" , "ключ" } };
 
-	InsertNewPair("êëàâèøà", "key", EmptyDictionary);
+	InsertNewPair("клавиша", "key", EmptyDictionary);
 	BOOST_CHECK(EmptyDictionary == dictionary);
 
-	InsertNewPair("êëþ÷", "key", dictionary);
+	InsertNewPair("ключ", "key", dictionary);
 	BOOST_CHECK(dictionary == UpdatedDictionary);
 }
 
@@ -116,13 +116,13 @@ BOOST_AUTO_TEST_SUITE(FillDictionaryFromInputStream_function)
 BOOST_AUTO_TEST_CASE(should_makes_empty_dictionary_from_empty_file)
 {
 	stringstream input;
-	Dictionary EmptyDictionary;
+	Dictionary emptyDictionary;
 
-	FillDictionaryFromInputStream(input, EmptyDictionary);
-	BOOST_CHECK(EmptyDictionary.empty());
+	FillDictionaryFromInputStream(input, emptyDictionary);
+	BOOST_CHECK(emptyDictionary.empty());
 }
 
-BOOST_AUTO_TEST_CASE(should_add_new_pair_to_non_empty_dictionary)
+BOOST_AUTO_TEST_CASE(should_fill_dictionary_from_file)
 {
 	stringstream input;
 	Dictionary dictionary =
