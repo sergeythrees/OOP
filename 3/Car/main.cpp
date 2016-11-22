ff0b0ba278ecdce87f6224502cd23c340940479f
sergeythrees <sergeythrees@gmail.com>
2016-11-22 21:45:24 +0300

    доработал диалог с пользователем

--- 3/Car/main.cpp
+++ 3/Car/main.cpp
@@ -10,6 +10,14 @@ int main()
 	CCar car;
 	CCarControl carControl(car, cin, cout);
 
+	cout << "Car and CarControl were initialized, please use these commands: " << endl
+		<< "Info - for print information of current car condition" << endl
+		<< "EngineOn  - for turn on car engine" << endl
+		<< "EngineOn  - for turn off car engine" << endl
+		<< "SetGear <value> - for change engine gear" << endl
+		<< "SetSpeed <value> - for change car speed" << endl;
+	cout << endl;
+
 	while (!cin.eof() && !cin.fail())
 	{
 		cout << "> ";
