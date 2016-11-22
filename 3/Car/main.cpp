a4021ae582b4b2eac3d9ececab0c7238633f7b47
sergeythrees <sergeythrees@gmail.com>
2016-11-22 21:29:44 +0300

    написал основной цикл программы

--- 3/Car/main.cpp
+++ 3/Car/main.cpp
@@ -1,8 +1,23 @@
 #include "stdafx.h"
+#include "Car.h"
+#include "CarControl.h"
 
+using namespace std;
 
 int main()
 {
-    return 0;
-}
+	setlocale(LC_ALL, "rus");
+	CCar car;
+	CCarControl carControl(car, cin, cout);
 
+	while (!cin.eof() && !cin.fail())
+	{
+		cout << "> ";
+		if (!carControl.HandleCommand())
+		{
+			cout << "Unknown command!\n";
+		}
+	}
+
+	return 0;
+}
