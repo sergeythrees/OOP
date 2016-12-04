1a5e9cb014835a33238c47016c07c3e8c77949c6
sergeythrees <sergeythrees@gmail.com>
2016-12-04 15:37:56 +0300

    добавил boost::optional

--- 5/task1tests/RationalTests.cpp
+++ 5/task1tests/RationalTests.cpp
@@ -3,6 +3,7 @@
 
 #include "stdafx.h"
 #include "../task1/Rational.h"
+#include <boost/optional.hpp>
 
 BOOST_AUTO_TEST_CASE(Test_Greates_Common_Denominator)
 {
@@ -28,16 +29,22 @@ void VerifyCompoundFraction(std::pair<int, CRational> const& result, int number,
 	BOOST_CHECK_EQUAL(result.second.GetDenominator(), expectedDenominator);
 }
 
-void VerifyInputOperator(std::string inputString, CRational expectedRational)
+void VerifyInputOperator(std::string inputString, boost::optional<CRational> expectedRational)
 {
 	std::stringstream input = std::stringstream();
 	input << inputString;
-	CRational result(expectedRational.GetNumerator(), expectedRational.GetDenominator());
-	if (!(input >> result))
+	CRational result(-3, 4);
+	input >> result;
+	if (expectedRational)
+	{
+		VerifyRational(result, expectedRational->GetNumerator(), expectedRational->GetDenominator());
+	}
+	else
 	{
 		BOOST_CHECK(input.fail());
+		VerifyRational(result, -3, 4);
 	}
-	VerifyRational(result, expectedRational.GetNumerator(), expectedRational.GetDenominator());
+	
 }
 
 BOOST_AUTO_TEST_SUITE(Rational_number)
@@ -417,13 +424,9 @@ BOOST_AUTO_TEST_SUITE(input_operator)
 		VerifyInputOperator("7/15", CRational(7, 15));
 		VerifyInputOperator("-7/15", CRational(-7, 15));
 	}
-	BOOST_AUTO_TEST_CASE(should_return_fail_if_can_not_read_rational_number)
+	BOOST_AUTO_TEST_CASE(should_not_change_rational_number_variable_and_return_fail_if_could_not_read)
 	{
-		VerifyInputOperator("1.15", CRational(-7, 15));
-	}
-	BOOST_AUTO_TEST_CASE(should_not_change_rational_number_if_input_return_fail)
-	{
-		VerifyInputOperator("1.15", CRational(0, 1));
+		VerifyInputOperator("1.15", boost::none);
 	}
 	BOOST_AUTO_TEST_SUITE_END()
 
