a51bdc1a89a2bba865967d0e82e25e9054056a4c
sergeythrees <sergeythrees@gmail.com>
2016-12-06 20:28:25 +0300

    реализовал функцию GeneratePrimeNumbersSet() на основе решета Эратосфена

--- 2/PrimeNumbers/PrimeNumbers.cpp
+++ 2/PrimeNumbers/PrimeNumbers.cpp
@@ -2,13 +2,39 @@
 #include "PrimeNumbers.h"
 
 using namespace std;
-
+void InitSieve(int upperBound, vector<bool>& sieve)
+{
+	sieve[0] = sieve[1] = false;
+	int sqrtUpperBound = static_cast<int>(round(sqrt(upperBound)));
+	for (int i = MIN_PRIME_NUMBER; i <= sqrtUpperBound; ++i)
+	{
+		if (sieve[i])
+		{
+			int speed = (i == 2) ? i : i + i;
+			for (int j = i * i; j <= upperBound; j += speed)
+			{
+				sieve[j] = false;
+			}
+		}
+	}
+}
 std::set<int> GeneratePrimeNumbersSet(int upperBound)
 {
 	set<int> result;
 	if (upperBound > 0 && upperBound <= MAX_UPPER_BOUND)
 	{
-
+		if (upperBound >= MIN_PRIME_NUMBER)
+		{
+			std::vector<bool> sieve(upperBound + 1, true);
+			InitSieve(upperBound, sieve);
+			for (int i = MIN_PRIME_NUMBER; i <= upperBound; ++i)
+			{
+				if (sieve[i])
+				{
+					result.emplace_hint(result.end(), i);
+				}
+			}
+		}
 	}
 	return result;
 }
--- 2/PrimeNumbersTests/PrimeNumbersTests.cpp
+++ 2/PrimeNumbersTests/PrimeNumbersTests.cpp
@@ -11,5 +11,14 @@ BOOST_AUTO_TEST_SUITE(GeneratePrimeNumbersSet_function)
 		BOOST_CHECK(GeneratePrimeNumbersSet(0).empty());
 		BOOST_CHECK(GeneratePrimeNumbersSet(100000001).empty());
 	}
+	BOOST_AUTO_TEST_CASE(makes_empty_set_if_upper_bound_is_less_than_minimal_prime_number)
+	{
+		BOOST_CHECK(GeneratePrimeNumbersSet(1).empty());
+	}
+	BOOST_AUTO_TEST_CASE(makes_prime_numbers_set_if_upper_bound_is_not_out_of_range)
+    {
+		BOOST_CHECK_EQUAL(GeneratePrimeNumbersSet(7).size(), 4);
+        BOOST_CHECK_EQUAL(GeneratePrimeNumbersSet(25).size(), 9);
+    }
 
 BOOST_AUTO_TEST_SUITE_END()
