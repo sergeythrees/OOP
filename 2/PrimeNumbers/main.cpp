533d849756d504f5d8daf01be4db33a94e189a02
sergeythrees <sergeythrees@gmail.com>
2016-12-06 19:46:21 +0300

    создал каркас проектаов PrimeNumbers и PrimeNumbersTests

---
+++ 2/PrimeNumbers/main.cpp
@@ -0,0 +1,9 @@
+#include "stdafx.h"
+#include "PrimeNumbers.h"
+
+using namespace std;
+
+int main()
+{
+	return 0;
+}
---
+++ 2/PrimeNumbersTests/PrimeNumbersTests.cpp
@@ -0,0 +1,13 @@
+#include "stdafx.h"
+#include "../PrimeNumbers/PrimeNumbers.h"
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(PrimeNumbers_function)
+
+		BOOST_AUTO_TEST_CASE(___)
+		{
+			BOOST_CHECK(true);
+		}
+
+BOOST_AUTO_TEST_SUITE_END()
