a7a369b505020ef29ec19bdf31b2d222c5b1e11d
sergeythrees <sergeythrees@gmail.com>
2016-12-06 21:14:18 +0300

    реализовал проверку входных данных в приложении

--- 2/PrimeNumbers/main.cpp
+++ 2/PrimeNumbers/main.cpp
@@ -1,9 +1,34 @@
 #include "stdafx.h"
 #include "PrimeNumbers.h"
 
+const unsigned ARG_COUNT = 2;
+
 using namespace std;
 
-int main()
+bool IsValidArgumentsCount(int argc)
 {
-	return 0;
+	return (argc == ARG_COUNT);
+}
+
+int main(int argc, char * argv[])
+{
+	int upperBound = 0;
+	if (!IsValidArgumentsCount(argc))
+	{
+		cout << "Неверные аргументы коммандной строки. Используйте :" << endl;
+		cout << "PrimeNumbersSetGenerator.exe <upperBound>" << endl;
+		cout << "Введите значение upperBound или пустую строку для завершения" << endl;
+		if (!(cin >> upperBound))
+			return EXIT_SUCCESS;
+	}
+	else
+	{
+		upperBound = atoi(argv[1]);
+	}
+
+
+
+
+
+	return EXIT_SUCCESS;
 }
