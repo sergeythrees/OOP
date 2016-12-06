53c036ac9ad25d3ee47ece92d788db90705a4778
sergeythrees <sergeythrees@gmail.com>
2016-12-06 23:14:25 +0300

    заменил функцию ParseUrl() на класс CUrl

--- 2/ParseUrl/ParseUrl.cpp
+++ 2/ParseUrl/ParseUrl.cpp
@@ -3,3 +3,30 @@
 
 using namespace std;
 
+CUrl::CUrl(string const & url)
+	: m_protocol()
+	, m_port()
+	, m_host()
+	, m_document()
+{
+}
+
+Protocol CUrl::protocol()
+{
+	return m_protocol;
+}
+
+unsigned short CUrl::port()
+{
+	return m_port;
+}
+
+string CUrl::host()
+{
+	return m_host;
+}
+
+string CUrl::document()
+{
+	return m_document;
+}
--- 2/ParseUrl/ParseUrl.h
+++ 2/ParseUrl/ParseUrl.h
@@ -1,2 +1,20 @@
 #pragma once
 #include "stdafx.h"
+
+enum Protocol {HTTP, HTTPS};
+
+class CUrl
+{
+public:
+	CUrl(string const& url);
+	Protocol protocol();
+	unsigned short port();
+	string host();
+	string document();
+
+private:
+	Protocol m_protocol = HTTP;
+	unsigned short m_port = 80;
+	string m_host;
+	string m_document;
+};
