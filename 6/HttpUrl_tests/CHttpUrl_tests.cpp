33ef988040fa7008b42f39d5d49d295ccde0e233
sergeythrees <sergeythrees@gmail.com>
2016-12-13 10:11:57 +0300

    вынес повторяющийся код в функцию

--- 6/HttpUrl_tests/CHttpUrl_tests.cpp
+++ 6/HttpUrl_tests/CHttpUrl_tests.cpp
@@ -1,5 +1,6 @@
 ﻿#include "stdafx.h"
 #include <boost/optional.hpp>
+#include <boost/optional/optional_io.hpp>
 #include <boost/variant.hpp>
 #include "../HttpUrl/HttpUrl.h"
 #include "../HttpUrl/CUrlParsingError.h"
@@ -38,36 +39,47 @@ struct CHttpUrlParameters
 	optional<unsigned short> port;
 };
 
-void ExpectCHttpUrl(variant<string, CHttpUrlParameters> parameters, const CHttpUrlParameters& expected)
+void ExpectCHttpUrlParameters(const CHttpUrlParameters& result, const CHttpUrlParameters& expected)
+{
+	BOOST_CHECK_EQUAL(result.domain, expected.domain);
+	BOOST_CHECK_EQUAL(result.document, expected.document);
+	if (result.port.is_initialized())
+		BOOST_CHECK_EQUAL(result.port, expected.port.get());
+	if (result.protocol.is_initialized())
+		BOOST_CHECK(result.protocol == expected.protocol.get());
+
+}
+
+void ExpectCHttpUrl(variant<string, CHttpUrlParameters> parameters, optional<const CHttpUrlParameters> expected)
 {
 	optional<CHttpUrl> url;
 	if (parameters.type() == typeid(string))
 	{
 		BOOST_CHECK_NO_THROW(url = CHttpUrl(get<string>(parameters)));
+		ExpectCHttpUrlParameters({ url.get().GetDomain(), url.get().GetDocument(), url.get().GetProtocol(), url.get().GetPort() }, expected.get());
 	}
 	else if (parameters.type() == typeid(CHttpUrlParameters))
 	{
 		CHttpUrlParameters p = get<CHttpUrlParameters>(parameters);
-		if (expected.port.is_initialized())
+		if (p.port.is_initialized())
 		{
 			BOOST_CHECK_NO_THROW(url = CHttpUrl(p.domain, p.document.c_str() + 1, p.protocol.get(), p.port.get()));
+			ExpectCHttpUrlParameters({ url.get().GetDomain(), url.get().GetDocument(), url.get().GetProtocol(), url.get().GetPort() }, p);
 		}
-		else if (!expected.port.is_initialized() && !expected.protocol.is_initialized())
+		else if (!p.port.is_initialized() && !p.protocol.is_initialized())
 		{
 			BOOST_CHECK_NO_THROW(url = CHttpUrl(p.domain, p.document.c_str() + 1));
+			ExpectCHttpUrlParameters({ url.get().GetDomain(), url.get().GetDocument()}, p);
 		}
 		else
 		{
 			BOOST_CHECK_NO_THROW(url = CHttpUrl(p.domain, p.document.c_str() + 1, p.protocol.get()));
+			ExpectCHttpUrlParameters({ url.get().GetDomain(), url.get().GetDocument(), url.get().GetProtocol()}, p);
 		}
+		
 	}
 
-	BOOST_CHECK_EQUAL(url.get().GetDomain(), expected.domain);
-	BOOST_CHECK_EQUAL(url.get().GetDocument(), expected.document);
-	if (expected.port.is_initialized())
-		BOOST_CHECK_EQUAL(url.get().GetPort(), expected.port.get());
-	if (expected.protocol.is_initialized())
-		BOOST_CHECK(url.get().GetProtocol() == expected.protocol.get());
+	
 		
 }
 
@@ -77,20 +89,20 @@ BOOST_AUTO_TEST_SUITE(HttpUrl_class)
 		BOOST_AUTO_TEST_CASE(can_be_costruct_from_correct_url_string)
 		{
 			ExpectCHttpUrl("http://www.mysite.com:100/docs/document1.html?page=30&lang=en#title", 
-							{ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::HTTP, 100 });
+				CHttpUrlParameters({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::HTTP, 100 }));
 		}
 		BOOST_AUTO_TEST_CASE(can_be_costruct_from_url_line_without_port_value)
 		{
 			ExpectCHttpUrl("http://www.mysite.com/docs/document1.html?page=30&lang=en#title", 
-							{ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::HTTP });
+				CHttpUrlParameters({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::HTTP, 80 }));
 		}
 		BOOST_AUTO_TEST_CASE(can_be_costruct_from_url_line_with_other_protocols)
 		{
 			ExpectCHttpUrl("https://www.mysite.com/docs/document1.html?page=30&lang=en#title",
-							{ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::HTTPS});
+				CHttpUrlParameters({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::HTTPS, 443}));
 
 			ExpectCHttpUrl("ftp://www.mysite.com/docs/document1.html?page=30&lang=en#title",
-			{ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::FTP });
+				CHttpUrlParameters({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::FTP, 21 }));
 		}
 		BOOST_AUTO_TEST_SUITE(should_return_approppriate_exceptions)
 			BOOST_AUTO_TEST_CASE(when_can_not_parse_URL_line)
@@ -129,18 +141,15 @@ BOOST_AUTO_TEST_SUITE(HttpUrl_class)
 	BOOST_AUTO_TEST_SUITE(constructor_from_parameters)
 		BOOST_AUTO_TEST_CASE(can_be_construct_from_correct_parameters)
 		{
-			CHttpUrlParameters expected({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::HTTP, 100 });
-			ExpectCHttpUrl(expected, expected);
+			ExpectCHttpUrl(CHttpUrlParameters({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" , Protocol::HTTP, 100 }), none);
 		}
 		BOOST_AUTO_TEST_CASE(can_be_construct_without_port_value)
-		{			
-			CHttpUrlParameters expected({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title", Protocol::HTTP});
-			ExpectCHttpUrl(expected, expected);
+		{
+			ExpectCHttpUrl(CHttpUrlParameters({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title", Protocol::HTTP }), none);
 		}
 		BOOST_AUTO_TEST_CASE(can_be_construct_without_port_and_protocol_value)
 		{
-			CHttpUrlParameters expected({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title"});
-			ExpectCHttpUrl(expected, expected);
+			ExpectCHttpUrl(CHttpUrlParameters({ "www.mysite.com", "/docs/document1.html?page=30&lang=en#title" }), none);
 		}
 		BOOST_AUTO_TEST_SUITE(should_return_approppriate_exceptions)
 			BOOST_AUTO_TEST_CASE(when_parameters_are_incorrect)
