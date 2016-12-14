e274ccdc776b217a4aff1cef2c955405163a99e6
sergeythrees <sergeythrees@gmail.com>
2016-12-14 10:58:29 +0300

    добавил вывод теста исключения в лог тестов

--- 6/HttpUrl_tests/CHttpUrl_tests.cpp
+++ 6/HttpUrl_tests/CHttpUrl_tests.cpp
@@ -12,6 +12,7 @@ template <typename Ex, typename Fn>
 void VerifyException(Fn && fn, const string & expectedDescription)
 {
 	BOOST_CHECK_EXCEPTION(fn(), Ex, [&](const Ex& e) {
+		BOOST_CHECK_EQUAL(e.what() , expectedDescription);
 		return e.what() == expectedDescription;
 	});
 }
