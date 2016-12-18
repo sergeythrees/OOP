ea40776ef06f3beb9e712556c5b31021c48d61a5
sergeythrees <sergeythrees@gmail.com>
2016-12-18 18:31:04 +0300

    набросал тесты к работе с вектором интов

--- 7/FindMaxEx_tests/FindMaxEx_tests.cpp
+++ 7/FindMaxEx_tests/FindMaxEx_tests.cpp
@@ -3,11 +3,39 @@
 
 using namespace std;
 
-BOOST_AUTO_TEST_SUITE(_)
+BOOST_AUTO_TEST_SUITE(FindMaxEx_function)
 
-	BOOST_AUTO_TEST_CASE(_)
+	struct NumbersFixture
 	{
-		BOOST_CHECK(true);
-	}
+		std::vector<int> numbers;
+		int maxNumber = 0;
+	};
+
+	BOOST_FIXTURE_TEST_SUITE(when_work_with_vector_of_int, NumbersFixture)
+
+		BOOST_AUTO_TEST_CASE(should_not_find_if_vector_is_empty)
+		{
+			BOOST_CHECK(!FindMaxEx(numbers, maxNumber, isgreater<int, int)());
+		}
+		BOOST_AUTO_TEST_CASE(should_find_if_vector_is_singleton)
+		{
+			numbers.push_back(10);
+			BOOST_CHECK(FindMaxEx(numbers, maxNumber, isgreater<int, int)());
+			BOOST_CHECK_EQUAL(maxNumber, 3);
+		}
+		BOOST_AUTO_TEST_CASE(can_find_max_number)
+		{
+			numbers.push_back(-3);
+			numbers.push_back(-8);
+			BOOST_CHECK(FindMaxEx(numbers, maxNumber, isgreater<int, int)());
+			BOOST_CHECK_EQUAL(maxNumber, -3);
+
+			numbers.push_back(0);
+			numbers.push_back(5);
+			BOOST_CHECK(FindMaxEx(numbers, maxNumber, isgreater<int, int)());
+			BOOST_CHECK_EQUAL(maxNumber, 5);
+		}
+	BOOST_AUTO_TEST_SUITE_END()
+
 
 BOOST_AUTO_TEST_SUITE_END()
