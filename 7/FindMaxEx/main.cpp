4a760c89781e5965ff494ca84a3df6c6bdb9ceb5
sergeythrees <sergeythrees@gmail.com>
2016-12-18 17:44:52 +0300

    создал проект

---
+++ 7/FindMaxEx/main.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include <locale>
+#include "FindMaxEx.h"
+
+using namespace std;
+
+int main()
+{
+	setlocale(LC_ALL, "rus");
+
+	return EXIT_SUCCESS;
+}
+
---
+++ 7/FindMaxEx_tests/FindMaxEx_tests.cpp
@@ -0,0 +1,13 @@
+﻿#include "stdafx.h"
+#include "../FindMaxEx/FindMaxEx.h"
+
+using namespace std;
+
+BOOST_AUTO_TEST_SUITE(_)
+
+	BOOST_AUTO_TEST_CASE(_)
+	{
+		BOOST_CHECK(true);
+	}
+
+BOOST_AUTO_TEST_SUITE_END()
