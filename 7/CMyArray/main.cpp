5bcd9165fcdf6ecd7c78a2cdfb51d7d58c6cc30d
sergeythrees <sergeythrees@gmail.com>
2017-01-13 09:30:58 +0300

    добавил в main() примеры работы с CMyArray<string> и CMyArray<double>

--- 7/CMyArray/main.cpp
+++ 7/CMyArray/main.cpp
@@ -7,6 +7,7 @@ int main()
 {
 	string inputLine;
 	CMyArray<string> stringArray;
+	cout << "Please input lines (Ctrl-Z to finish):" << endl;
 	while (cin >> inputLine)
 	{
 		stringArray.Append(inputLine);
@@ -17,6 +18,23 @@ int main()
 	}
 	cout << endl;
 	for (auto it = stringArray.rbegin(); it != stringArray.rend(); ++it)
+	{
+		cout << *it << endl;
+	}
+	cin.clear();
+	double inputNumber;
+	CMyArray<double> numbersArray;
+	cout << "Please input numbers (Ctrl-Z to finish):" << endl;
+	while (cin >> inputNumber)
+	{
+		numbersArray.Append(inputNumber);
+	}
+	for (auto it = numbersArray.begin(); it != numbersArray.end(); ++it)
+	{
+		cout << *it << endl;
+	}
+	cout << endl;
+	for (auto it = numbersArray.rbegin(); it != numbersArray.rend(); ++it)
 	{
 		cout << *it << endl;
 	}
